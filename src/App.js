383da9fb6d28fa0161cd309d1f26f665cad60f40
panchocandioti <panchocandioti@gmail.com>
2023-12-30 01:05:44 -0300

    Diapos, descripcion y generalidades

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import MiLecheria from './multimedia/Logo MiLecheria.jpg';
+import OLE from './multimedia/OLE.jpg';
+import rodeo from './multimedia/crecimiento_rodeo.jpg'
 import './App.css';
 
 function App() {
@@ -9,16 +11,58 @@ function App() {
       </header>
       <body>
         <div className='seccion'>
-          REGISTRARSE
+          <h2>¡Hola, qué bueno que estés aquí!</h2>
+          <div className='descripcion'>
+            <p>En este sitio encontrarás herramientas simples para la gestión de establecimientos lecheros sin importar su tamaño.</p>
+            <p>Para poder notificarte de novedades y actualizaciones te sugiero <a href='https://forms.gle/Q12JUazDxuxbRSP7A' target="_blank" rel="noopener noreferrer">registrarte</a>.</p>
+            <p>Más abajo podrás acceder a las aplicaciones disponibles que serán cada vez más.</p>
+            <p>Espero que aprendas a utilizarlas y te sirvan mucho en tu formación y en tu trabajo.</p>
+            <p><b><a href='https://www.linkedin.com/in/francisco-candioti-0b167834/' target="_blank" rel="noopener noreferrer">Ing. Agr. EPL Francisco Candioti</a></b>   <a href='mailto:panchocandioti@gmail.com'>panchocandioti@gmail.com</a></p>
+          </div>
+
         </div>
         <div className='seccion'>
-          OLE!5.5
+          <h2>OLE! 5.5</h2>
+          <img src={OLE} className='diapo' alt='OLE'></img>
+          <div className='descripcion'>
+            <h3>Descripción:</h3>
+            <p>Modelo de simulación de sistema de producción completo. Para utilizar en la etapa de planificación.</p>
+            <p>Con datos de entrada de obtención sencilla estima resultados físicos, económicos, sociales y ambientales.</p>
+            <p>Permite evaluar en profundidad muchas estrategias alternativas en poco tiempo antes de su puesta en práctica.</p>
+            <p>Desarrollado en el marco del proyecto <a href='https://www.fontagro.org/new/proyectos/lecheria-sostenible/es' target="_blank" rel="noopener noreferrer">LACTIS</a>. Se adapta a diferentes sistemas de producción de leche.</p>
+          </div>
+          <div className='generalidades'>
+            <ul><li><b>Requerimientos:</b> Excel 2010 o superior</li>
+              <li><b>Dispositivos:</b> ordenador</li>
+              <li><b>Tipo de licencia:</b> gratuita</li>
+              <li><b>Videos tutoriales:</b> disponibles</li>
+              <li><b>Guía del usuario:</b> disponible</li>
+              <li><b>Ayudas rápidas: </b> no disponibles</li>
+            </ul>
+          </div>
+          <br></br>
         </div>
         <div className='seccion'>
-          MONITOR LECHERO
-        </div>
-        <div className='seccion'>
-          CRECIMIENTO DEL RODEO LECHERO
+          <h2>CRECIMIENTO DEL RODEO LECHERO</h2>
+          <img src={rodeo} className='diapo' alt='crecimiento_rodeo'></img>
+          <div className='descripcion'>
+            <h3>Descripción:</h3>
+            <p>Aplicación web que permite calcular el crecimiento genuino del rodeo lechero (sin compra de vacas ni vaquillonas).</p>
+            <p>Simula su evolución a cinco años. Permite evaluar cómo impactan determinados cambios en los parámetros del rodeo sobre su evolución. </p>
+            <p>Compara escenarios alternativos con la situación original. Emite reportes para descargar en formato PDF.</p>
+          </div>
+          <div className='generalidades'>
+            <ul><li><b>Requerimientos:</b> navegador web</li>
+              <li><b>Dispositivos:</b> cualquiera</li>
+              <li><b>Tipo de licencia:</b> gratuita</li>
+              <li><b>Videos tutoriales:</b> no disponibles</li>
+              <li><b>Guía del usuario:</b> no disponible</li>
+              <li><b>Ayudas rápidas: </b> disponibles</li>
+            </ul>
+          </div>
+          <br></br>
+          <a href='https://panchocandioti.github.io/crecimiento-rodeo/' target="_blank" rel="noopener noreferrer">Ir a la aplicación</a>
+          <br></br>
         </div>
       </body>
     </div>
