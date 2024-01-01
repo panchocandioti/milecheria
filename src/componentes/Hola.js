6d6d09ac053666b888e0b9470dbd666bf19ad4ab
panchocandioti <panchocandioti@gmail.com>
2024-01-01 19:07:49 -0300

    Texto

--- src/componentes/Hola.js
+++ src/componentes/Hola.js
@@ -7,7 +7,7 @@ function Hola() {
                 <h2>¡Hola, qué bueno que estés aquí!</h2>
                 <div className='descripcion'>
                     <div className='texto'>
-                        <p>En este sitio encontrarás herramientas simples para la gestión de establecimientos lecheros sin importar su tamaño.</p>
+                        <p>En este sitio encontrarás herramientas simples para la gestión profesional de establecimientos lecheros sin importar su tamaño.</p>
                         <p>Para poder notificarte de novedades y actualizaciones te sugiero <a href='https://forms.gle/Q12JUazDxuxbRSP7A' target="_blank" rel="noopener noreferrer">registrarte</a>.</p>
                         <p>Más abajo podrás acceder a las aplicaciones disponibles que serán cada vez más.</p>
                         <p>Espero que aprendas a utilizarlas y te sirvan mucho en tu formación y en tu trabajo.</p>
